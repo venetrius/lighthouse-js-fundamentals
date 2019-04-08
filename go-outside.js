6ce33b2d3dcf4541d59aa1693fe7b0a18b59e9f4
venetrius <gergelygjuhasz@gmail.com>
2019-04-08 02:32:18 +0000

    exercise regarding conditional statments (if)

---
+++ go-outside.js
@@ -0,0 +1,20 @@
+var temperature = 12;
+var raining = false;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
